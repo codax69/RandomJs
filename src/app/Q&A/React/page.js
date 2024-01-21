ce0bda9d0558040e8b77ea7b1dc30c7b547edbaa
codax69 <128021116+codax69@users.noreply.github.com>
2024-01-21 15:07:34 +0530

    issue sloved

--- src/app/Q&A/React/page.js
+++ src/app/Q&A/React/page.js
@@ -11,7 +11,7 @@ const page = () => {
         <div className='relative lg:w-60 lg:h-screen'>
           <Sidebar />
         </div>
-        <div className='absolute w-auto h-auto top-16 lg:p-20 lg:fixed lg:left-[23rem] '>
+        <div className='absolute w-auto h-auto top-16 lg:p-20 lg:absolute lg:left-[23rem] '>
           <RQnABox />
         </div>
       </div></>
